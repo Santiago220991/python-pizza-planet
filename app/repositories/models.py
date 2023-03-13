48001be91006db386827f3b2aa22d805710f27dc
Santiago220991 <alexander.cardenas10@outlook.com>
2023-03-13 14:52:10 -0400

    feat: add Beverage and OrderBeverageDetail models

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -15,6 +15,7 @@ class Order(db.Model):
 
     size = db.relationship('Size', backref=db.backref('size'))
     detail = db.relationship('OrderDetail', backref=db.backref('order_detail'))
+    beverage = db.relationship('OrderBeverageDetail', backref=db.backref('order_beverage_detail'))
 
 
 class Ingredient(db.Model):
@@ -29,9 +30,22 @@ class Size(db.Model):
     price = db.Column(db.Float, nullable=False)
 
 
+class Beverage(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
 class OrderDetail(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     ingredient_price = db.Column(db.Float)
     order_id = db.Column(db.Integer, db.ForeignKey('order._id'))
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient._id'))
     ingredient = db.relationship('Ingredient', backref=db.backref('ingredient'))
+
+class OrderBeverageDetail(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    beverage_price = db.Column(db.Float)
+    order_id = db.Column(db.Integer, db.ForeignKey('order._id'))
+    beverage_id = db.Column(db.Integer, db.ForeignKey('beverage._id'))
+    beverage = db.relationship('Beverage', backref=db.backref('beverage'))
